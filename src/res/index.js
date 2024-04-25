b19db5dce853075fb9bd7c93ba1b2e96dbd0c4fe
Mick Sayson <mick@sayson.com>
2024-04-25 11:51:44 -0700

    Randomize cluster id colors
    Clustering algorithms can produce some form of structure in point
    location -> cluster id. Randomize the colors to ensure that we don't
    initialize in a state that looks clustered when it isn't

--- src/res/index.js
+++ src/res/index.js
@@ -1,16 +1,28 @@
-function computeColor(cluster_id, num_clusters) {
-  if (cluster_id >= num_clusters) {
-    throw new Error("Invalid cluster id");
+var cluster_colors = [];
+
+// Clusters can have some initial structure, which results in similar positions
+// naturally having similar colors if we just map cluster id to hue. We store
+// cluster colors and randomize the order to ensure a random distribution
+function resetClusterColors(num_clusters) {
+  cluster_colors = [];
+  for (let i = 0; i < num_clusters; i++) {
+    const hue = (i / num_clusters) * 360;
+    cluster_colors.push("hsla(" + Math.floor(hue) + ", 100%, 50%, 1.0)");
   }
-  const hue = (cluster_id / (num_clusters - 1)) * 360;
-  const ret = "hsla(" + Math.floor(hue) + ", 100%, 50%, 1.0)";
-  return ret;
+
+  // Yoinked from stack overflow but makes sense
+  cluster_colors = cluster_colors
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
 }
 
 async function rerender() {
   const response = await fetch("/data");
   const data = await response.json();
-
+  if (cluster_colors.length !== data.clusters.length) {
+    resetClusterColors(data.clusters.length);
+  }
   /** @type HTMLCanvasElement */
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
@@ -20,7 +32,7 @@ async function rerender() {
   const canvas_offset = 25;
 
   for (let cluster_id = 0; cluster_id < data.clusters.length; cluster_id++) {
-    const color = computeColor(cluster_id, data.clusters.length);
+    const color = cluster_colors[cluster_id];
     for (const point_id of data.clusters[cluster_id]) {
       const point = data.points[point_id];
       ctx.beginPath();
